a7a0109a55edbc796b06c69bf81ecb8cd911fa86
lpaulovt <silvavitorlopes711@gmail.com>
2021-03-23 22:30:15 -0300

    :sparkles: New challenges

---
+++ PEX1243/lab-algt-03.c
@@ -0,0 +1,50 @@
+/* Aluno: 
+   Paulo Vitor Lopes da Silva - Matrícula: 2020010310 - email: paulo.silva06497@alunos.ufersa.edu.br
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void checksLargerNumbers(int v[], int n, int *pCompareNumber)
+{
+  printf("Os valores maiores ou iguais a 3 no vetor são:\n");
+  for (int i = 0; i < n; i++)
+  {
+    if (v[i] >= *pCompareNumber)
+    {
+      printf("%d\n", v[i]);
+    }
+  }
+}
+
+int main()
+{
+  int arraySize;
+  int *pArraySize = &arraySize;
+
+  printf("Digite o tamanho do vetor:\n");
+  scanf("%d", pArraySize);
+
+  int *arrayNumbers = (int *)malloc(*pArraySize * sizeof(int));
+  if (arrayNumbers == NULL)
+  {
+    printf("Memória insuficiente");
+    exit(1);
+  }
+
+  printf("Digite os valores do vetor:\n");
+  for (int i = 0; i < *pArraySize; i++)
+  {
+    scanf("%d", &arrayNumbers[i]);
+  }
+
+  int compareNumber;
+  int *pCompareNumber = &compareNumber;
+
+  printf("Digite um valor a ser avaliado:\n");
+  scanf("%d", pCompareNumber);
+
+  checksLargerNumbers(arrayNumbers, *pArraySize, pCompareNumber);
+
+  free(arrayNumbers);
+}
